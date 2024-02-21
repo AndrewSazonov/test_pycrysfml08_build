ad6472afe37f9c4074cc6a43af03c0edf1fcf0e9
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-02-21 21:54:34 +0100

    Fix test macOS

--- tests/test_py_cfml_profiles.py
+++ tests/test_py_cfml_profiles.py
@@ -1,15 +1,18 @@
 import os
 import sys
+import math
 from numpy.testing import assert_almost_equal
 
-sys.path.append(os.path.join('PyCrysFML08_install','pycrysfml08'))
+sys.path.append(os.path.abspath('PyCrysFML08_install'))
 from pycrysfml08 import py_cfml_profiles
 
 # Tests
 
 def test_exponential():
-    alfa = 1.0
+    a = 1.0
     x = 1.5
-    desired = alfa * math.exp(-alfa * x)
-    actual = py_cfml_profiles.exponential(x, alfa)[2]
+    desired = a * math.exp(-a * x)
+    code, message, actual = py_cfml_profiles.exponential(x, a)
+    assert code == 0
+    assert message == ''
     assert_almost_equal(desired, actual, decimal=3, verbose=True)
