c83686b03ba813fcce20a9b6164a78cc1af71f31
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-02-27 14:45:29 +0100

    Fix powder_mod test

--- tests/test_powder_mod.py
+++ tests/test_powder_mod.py
@@ -147,10 +147,18 @@ def test_compute_pattern_SrTiO3_Pm3m():
     pattern = compute_pattern(study_dict)
     actual = pattern[1].astype(np.float64)
     # compensate for a 1-element shift in y data between Nebil windows build and my gfortran build
-    desired = y_expected[1:]
-    actual = y_calc[:-1]
+    desired = desired[1:]
+    actual = actual[:-1]
     assert_almost_equal(desired, actual, decimal=0, verbose=True)
 
+def test_compute_pattern_SrTiO3_Pnma():
+    desired = np.loadtxt('tests/srtio3-pmmm-pattern_Andrew-ifort.xy', unpack=True)
+    study_dict = copy.deepcopy(STUDY_DICT)
+    set_space_group_by_phase_idx(study_dict, phase_idx=0, space_group='P n m a')
+    clean_after_compute(study_dict)
+    pattern = compute_pattern(study_dict)
+    y_calc = pattern[1].astype(np.float64)
+    assert_almost_equal(y_expected, y_calc, decimal=0, verbose=True)
 
 # Debug
 
